8b0754d3d85e49fbf07ff57c499d083fae963be0
leilatl <leila.tolegenova@users.noreply.github.com>
2020-03-25 23:12:06 +0600

    hello

---
+++ api/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return '{}: {}'.format(self.id, self.name)
+    
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+        }
+
---
+++ api/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+from api.models import Product, Category
+from django.http.response import HttpResponse, JsonResponse
+
+
+def product_list(request):
+    try:
+        products = Product.objects.all()
+        products_json = [product.to_json() for product in products]
+        return JsonResponse(products_json, safe=False)
+    except:
+        return JsonResponse({ "error": "No products found in the DB..." })
+
+def product_detail(request, id):
+    try:
+        product = Product.objects.get(id = id)
+        product_json = product.to_json()
+        return JsonResponse(product_json, safe=False)
+    except:
+        return JsonResponse({ "error": "No such product found in the DB..." })
+
+def category_list(request):
+    try:
+        categories = Category.objects.all()
+        categories_json = [category.to_json() for category in categories]
+        return JsonResponse(categories_json, safe=False)
+    except:
+        return JsonResponse({ "error": "No categories found in the DB..." })
+
+def category_detail(request, id):
+    try:
+        category = Category.objects.get(id = id)
+        category_json = category.to_json()
+        return JsonResponse(category_json, safe=False)
+    except:
+        return JsonResponse({ "error": "No such category found in the DB..." })
+
+def products_by_category(request, id):
+    try:
+        products = Product.objects.get(category = id)
+        products_json = [product.to_json() for product in products]
+        return JsonResponse(products_json, safe=False)
+    except:
+        return JsonResponse({ "error": "No products found in the DB..." })
